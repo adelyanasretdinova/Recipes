6114cf8305584082d3e7e04b96b4a4ee9069b5a6
adelya <adelya.nasretdinova@gmail.com>
2024-10-05 11:17:12 +0200

    passing the dish to the ui, image is not displayed at the preview, needs fixing

--- app/src/main/java/com/example/recepies/ui/pdp/Pdp.kt
+++ app/src/main/java/com/example/recepies/ui/pdp/Pdp.kt
@@ -4,48 +4,27 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.view.WindowId
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.activity.viewModels
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Card
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.ColorFilter
-import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.ComposeView
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.testTag
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.fragment.app.Fragment
-import coil.compose.AsyncImage
 import coil.compose.rememberAsyncImagePainter
-import coil.request.ImageRequest
-import com.example.recepies.R
 import com.example.recepies.model.Dish
-import com.example.recepies.ui.main_screen.LazyGridContainers
-import com.example.recepies.ui.main_screen.MyAppScreen
-import com.example.recepies.ui.main_screen.Title
 import com.example.recepies.ui.theme.RecepiesTheme
 
 
@@ -67,39 +46,40 @@ class Pdp :  Fragment() {
                 val idProduct = arguments?.getString(PRODUCT_ID_ARG)
 
                 if (idProduct != null) {
-                    OneDishCard(id = idProduct)
+                    viewModel.loadOneDish(idProduct)
+                    OneDishCard(dish = viewModel.oneDishData()!!)}
                 }
+
             }
         }
     }
-}
 
 @Composable
-fun OneDishCard(id: String) {
+fun OneDishCard(dish: Dish) {
     Box(
         modifier = Modifier
             .padding(4.dp)
-            .fillMaxWidth()
+            .fillMaxSize()
     ) {
         Image(
-            painter = rememberAsyncImagePainter("http://t3.gstatic.com/licensed-image?q=tbn:ANd9GcRfcwEgXgk8f5BjnHpeLQYXYKZdjWDOJ_c5AY78C-mkkN0rkPTG2IduEiHJKeGQae5BhuCliCbJBWxibtn-BYw"),
-            contentDescription = null,
+            painter = rememberAsyncImagePainter(dish.image),
+            contentDescription = "dishImage",
             modifier = Modifier
                 .testTag("Image")
-                .fillMaxSize()
-                .clip(RoundedCornerShape(8.dp))
-                .align(Alignment.CenterStart),
-            contentScale = ContentScale.FillWidth,
-            alpha = 0.5F
+                .fillMaxWidth()
+//                .clip(RoundedCornerShape(8.dp))
+                .align(Alignment.TopCenter),
+        contentScale = ContentScale.FillWidth,
+        alpha = 0.5F
         )
         Column(
             modifier = Modifier
-                .fillMaxSize()
                 .align(Alignment.BottomCenter),
         ) {
-            Text(text = "Title", fontSize = 20.sp)
-            Text(text = "Ingredients", fontSize = 10.sp)
-            Text(text = "Description", fontSize = 10.sp)
+
+            Text(text = dish.name, fontSize = 20.sp)
+            Text(text = dish.ingredients, fontSize = 10.sp)
+            Text(text = dish.description, fontSize = 10.sp)
         }
     }
 }
@@ -107,6 +87,5 @@ fun OneDishCard(id: String) {
 @Preview(showBackground = true)
 @Composable
 fun OneDishCardPreview() {
-        OneDishCard(id = "1"
-        )
+        OneDishCard(dish = Dish(name = "Name1", ingredients = "Der Standardteil von Lorem Ipsum, genutzt seit 1500, ist reproduziert für die, die es interessiert. Sektion 1.10.32 und 1.10.33 von \"de Finibus Bonorum et Malroum\" von Cicero sind auch reproduziert in ihrer Originalform, abgeleitet von der Englischen Version aus von 1914 (H. Rackham)", description = "Glauben oder nicht glauben, Lorem Ipsum ist nicht nur ein zufälliger Text. Er hat Wurzeln aus der Lateinischen Literatur von 45 v. Chr, was ihn über 2000 Jahre alt macht. Richar McClintock, ein Lateinprofessor des Hampden-Sydney College in Virgnia untersuche einige undeutliche Worte, \"consectetur\", einer Lorem Ipsum Passage und fand eine unwiederlegbare Quelle. Lorem Ipsum komm aus der Sektion 1.10.32 und 1.10.33 des \"de Finibus Bonorum et Malorum\" (Die Extreme von Gut und Böse) von Cicero, geschrieben 45 v. Chr. Dieses Buch ist Abhandlung der Ethiktheorien, sehr bekannt wärend der Renaissance. Die erste Zeile des Lorem Ipsum, \"Lorem ipsum dolor sit amet...\", kommt aus einer Zeile der Sektion 1.10.32.", image = "http://t3.gstatic.com/licensed-image?q=tbn:ANd9GcRfcwEgXgk8f5BjnHpeLQYXYKZdjWDOJ_c5AY78C-mkkN0rkPTG2IduEiHJKeGQae5BhuCliCbJBWxibtn-BYw"))
 }
